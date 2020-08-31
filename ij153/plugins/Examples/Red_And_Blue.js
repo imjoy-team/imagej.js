df08c66ce83780860f527f3d28c83af5ddb4ebea
oeway <oeway@users.noreply.github.com>
2020-08-31 21:24:10 +0000

    Deploy to GitHub Pages 4aeb00181238b16667316322a3181f0e72532f3b

---
+++ ij153/plugins/Examples/Red_And_Blue.js
@@ -0,0 +1,12 @@
+/*Compiled using CheerpJ (R) 2.1 by Leaning Technologies Ltd*/
+cheerpjCL={cl:null};
+var N12Red_And_BlueG;
+function N12Red_And_Blue(){N4java4lang6Object.call(this);}
+N12Red_And_Blue.cl=cheerpjCL;
+function N12Red_And_BlueX(a){a.f=N12Red_And_BlueX;if(cheerpjSafeInitGuard('N12Red_And_Blue')) return;var q=a.q=N12Red_And_Blue.prototype=Object.create(N4java4lang6Object.prototype);q.constructor=N12Red_And_Blue;q.ifs=["ij/plugin/PlugIn"];q.dhr1ir_c79c=q.v11=_c4pJsVsm8R1m8VTqrChr14vd;N12Red_And_BlueG=1;cheerpjSafeInitFinish('N12Red_And_Blue');}
+var N2ij2IJG;
+var N2ij9ImagePlusG;
+var N2ij7process14ColorProcessorG;
+var N4java4lang6ObjectG;
+var N4java4lang13StringBuilderG;
+function _c4pJqVsm8R1m8VTqrWz7rd(d,p){_n4VYObjectC2E0(d,p);}function _c4pJsVsm8R1m8VTqrChr14vd(r,q,p){var d=0,j=0,g=0,h=0,m=0,l=0,c=null,e=null,k=0;var a={p:p,pc:0,f:_c4pJsVsm8R1m8VTqrChr14vd,m:0,l:0,c:null,e:null};a.pc=-155;a.m=m=_o4_2ystem17currentTimeMillisE14(a)|0;a.l=l=hSlot|0;a.pc=-223;;if((N2ij7process14ColorProcessorG|0)==0){cjG(a);};a.c=c=new N2ij7process14ColorProcessor();a.pc=-325;_c4_02ij7process14ColorPg1Ga3kGac7vd(c,400,400,a);a.pc=-385;a.e=e=c.v127(c,a);a.pc=32;;if(e&&!(e[0]==73)){cjCastFailure(a,e);};j=0;d=0;while(1){k=(Math.imul(d,255)>>>0)/399<<16;h=j;g=0;while(1){h=h+1|0;e[h]=(k|(Math.imul(g,255)>>>0)/399| -16777216);g=g+1|0;if(g>>>0<=399)continue;break;}d=d+1|0;if(d>>>0>399){a.pc=-644;;if((N2ij9ImagePlusG|0)==0){cjG(a);};a.e=e=new N2ij9ImagePlus();a.pc=-718;_c4F02ij9ImagePlusC2E2(e,cheerpjInternString("Red and Blue"),c,a);a.pc=-794;e.v35(e,a);a.pc=-815;;if((N4java4lang13StringBuilderG|0)==0){cjG(a);};a.c=c=new N4java4lang13StringBuilder();a.pc=-913;_m4FJge_YBuilderC2E0(c,a);a.pc=-949;a.c=c=c.v71(c,cheerpjInternString(""),a);a.pc=-1000;d=_o4_2ystem17currentTimeMillisE14(a)|0;d=sub64(d>>>0,(hSlot|0)>>>0,m>>>0,l>>>0)|0;a.pc=-1094;c=c.v80(c,d,(hSlot|0),a);a.pc=-1130;a.c=c=c.v4(c,a);a.pc=-1157;;if((N2ij2IJG|0)==0){cjG(a);};_c4F12ij2IJ10showStatusE27(c,p);return ;}j=j+400|0;continue;}}
