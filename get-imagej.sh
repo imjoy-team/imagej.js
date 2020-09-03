28796412069e1bb02de27819a8d023a692584431
Wei Ouyang <oeway007@gmail.com>
2020-09-03 14:20:10 +0200

    Change repo name

--- get-imagej.sh
+++ get-imagej.sh
@@ -1,7 +1,7 @@
 # the compiled files will be saved in ./dist
 set -e
 
-curl -s https://api.github.com/repos/imjoy-team/ImageJA/releases/latest | \
+curl -s https://api.github.com/repos/imjoy-team/ImageJA.JS/releases/latest | \
 grep "/imagej-js-dist.tgz" | \
 cut -d : -f 2,3 | \
 tr -d \" | \
