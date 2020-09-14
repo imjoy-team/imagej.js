d713a2dc507daca22be2fa9b7e40ba545ebfcc91
oeway <oeway@users.noreply.github.com>
2020-09-14 21:24:05 +0000

    Deploy to GitHub Pages 61e09ea135c56672ed6316984178570452a15210

--- ij153/plugins/Examples/Image_Inverter.js
+++
@@ -1,8 +0,0 @@
-/*Compiled using CheerpJ (R) 2.1 by Leaning Technologies Ltd*/
-cheerpjCL={cl:null};
-var N14Image_InverterG;
-function N14Image_Inverter(){N4java4lang6Object.call(this);}
-N14Image_Inverter.cl=cheerpjCL;
-function N14Image_InverterX(a){a.f=N14Image_InverterX;if(cheerpjSafeInitGuard('N14Image_Inverter')) return;var q=a.q=N14Image_Inverter.prototype=Object.create(N4java4lang6Object.prototype);q.constructor=N14Image_Inverter;q.ifs=["ij/plugin/filter/PlugInFilter"];q.ce$Wtupt0_Z2ij9ImagePlusEI=q.v11=_c4VJwlYayq8l2urenreLlrmr94vd;q.xhr1CAJKS_e5iqijvJlYayqKAudcCRWEV=q.v12=_c4VJulYayq8l2urenreDhr14zd;N14Image_InverterG=1;cheerpjSafeInitFinish('N14Image_Inverter');}
-var N4java4lang6ObjectG;
-function _c4VJslYayq8l2urenreXz7rd(d,p){_n4VYObjectC2E0(d,p);}function _c4VJwlYayq8l2urenreLlrmr94vd(j,h,g,p){return 127|0;}function _c4VJulYayq8l2urenreDhr14zd(l,k,p){var g=0,d=0,c=null,e=0;var a={p:p,pc:0,f:_c4VJulYayq8l2urenreDhr14zd,g:0,d:0,k:k,c:null};a.pc=-133;a.c=c=k.v60(k,a);g=c.y1|0;d=c.y1|0;if((g|0)<((c.height3|0)+d|0)){a.g=g=g;while(1){d=c.x0|0;e=c.x0|0;if((d|0)<((c.width2|0)+e|0)){a.d=d=d;while(1){a.pc=-289;e=k.v130(k,d,g,a)|0;a.pc=-319;k.v132(k,d,g,(e^ -1),a);e=d+1|0;d=c.x0|0;if((e|0)<((c.width2|0)+d|0)){a.d=d=e;continue;}break;}}e=g+1|0;g=c.y1|0;if((e|0)<((c.height3|0)+g|0)){a.g=g=e;continue;}break;}}}
--- ij153/plugins/Examples/_Scripts/About_These_Scripts.js
+++
@@ -1,5 +0,0 @@
-  path = IJ.getDirectory("plugins")+"Examples/_Scripts/About Scripts";
-  if (!(new File(path)).exists())
-     IJ.error("\"About Scripts\" not found in ImageJ/plugins/Examples/_Scripts.");
-  IJ.open(path);
-
