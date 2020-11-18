03f206ef726b3a38df0db73c09282da97e9ff2aa
Wei Ouyang <oeway007@gmail.com>
2020-11-18 16:06:16 +0100

    debounce the url encoding

--- src/index.js
+++ src/index.js
@@ -51,15 +51,35 @@ window.onEditorResized = () => {
   }, 1);
 };
 
-window.onEditorTextChanged = (name, content) => {
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+window.onEditorTextChanged = debounce((name, content) => {
   // update the sharing url
-  if (name === sharingScript.name) {
+  if (sharingScript && name === sharingScript.name) {
     const compressed = LZString.compressToEncodedURIComponent(
       JSON.stringify({ name, content })
     );
     insertUrlParam("open", compressed);
   }
-};
+}, 1000);
 
 function insertUrlParam(key, value) {
   if (history.pushState) {
@@ -1176,6 +1196,7 @@ async function processUrlParameters(imagej) {
         const decompressed = LZString.decompressFromEncodedURIComponent(url);
         if (decompressed) {
           const data = JSON.parse(decompressed);
+          sharingScript = data;
           const blob = new Blob([data.content]);
           const file = new File([blob], data.name, {
             type: "text/plain"
