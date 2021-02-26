5bf7f2b049df2ade2a4f40eaaddb8786abcd42b9
Wei Ouyang <oeway007@gmail.com>
2021-02-26 21:51:38 +0100

    Fix plugins

--- get-imagej.sh
+++ get-imagej.sh
@@ -1,6 +1,12 @@
 # the compiled files will be saved in ./dist
 set -e
 
+
+# download imagej 1.53g to make the transition smoother
+curl -OL https://github.com/imjoy-team/ImageJA.JS/releases/download/1.53g/imagej-js-dist.tgz
+tar -xvzf imagej-js-dist.tgz
+
+# download the latest imagej
 curl -s https://api.github.com/repos/imjoy-team/ImageJA.JS/releases/latest | \
 grep "/imagej-js-dist.tgz" | \
 cut -d : -f 2,3 | \
@@ -9,10 +15,6 @@ xargs -I {} curl -OL {}
 
 tar -xvzf imagej-js-dist.tgz
 rm imagej-js-dist.tgz
-
-# download imagej 1.53f to make the transition smoother
-curl -OL https://github.com/imjoy-team/ImageJA.JS/releases/download/1.53f/imagej-js-dist.tgz
-tar -xvzf imagej-js-dist.tgz
 
 mkdir -p dist
 
