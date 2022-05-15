de597e3f7acd602e8aced234cf768593da8a4bf8
oeway <oeway007@gmail.com>
2022-05-15 09:38:11 +0200

    Download 1.53m explictly

--- get-imagej.sh
+++ get-imagej.sh
@@ -6,14 +6,10 @@ set -e
 curl -OL https://github.com/imjoy-team/ImageJA.JS/releases/download/1.53j/imagej-js-dist.tgz
 tar -xvzf imagej-js-dist.tgz
 
-# download the latest imagej
-curl -s https://api.github.com/repos/imjoy-team/ImageJA.JS/releases/latest | \
-grep "/imagej-js-dist.tgz" | \
-cut -d : -f 2,3 | \
-tr -d \" | \
-xargs -I {} curl -OL {}
-
+# download imagej 1.53m to make the transition smoother
+curl -OL https://github.com/imjoy-team/ImageJA.JS/releases/download/1.53m/imagej-js-dist.tgz
 tar -xvzf imagej-js-dist.tgz
+
 rm imagej-js-dist.tgz
 
 mkdir -p dist
